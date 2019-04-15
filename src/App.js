475928dccb9eabc08a76cb7831a10bb669f9a5bb
Aleksandar M <salac@kolosek.com>
2019-04-15 14:59:49 +0200

    Updates 1

--- src/App.js
+++ src/App.js
@@ -2,13 +2,31 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import createApp, {getShopOrigin} from '@shopify/app-bridge';
+import {Redirect} from '@shopify/app-bridge/actions';
 
-const app = createApp({
-  apiKey: '16d6ed5795e888c6d9f5c98e64ae8697',
-  shopOrigin: 'salacteststore.myshopify.com',
-});
+const apiKey = '16d6ed5795e888c6d9f5c98e64ae8697';
+const redirectUri = 'https://app-bridge-testing.herokuapp.com/';
+const shopOrigin = 'salacteststore.myshopify.com';
+const permissionUrl = `/oauth/authorize?client_id=${apiKey}&scope=read_products,read_content&redirect_uri=${redirectUri}`;
 
 class App extends Component {
+
+  handleButtonClick(event) {
+    // If the current window is the 'parent', change the URL by setting location.href
+    if (window.top == window.self) {
+      window.location.assign(`https://${shopOrigin}/admin${permissionUrl}`)
+
+    // If the current window is the 'child', change the parent's URL with Shopify App Bridge's Redirect action
+    } else {
+      const app = createApp({
+        apiKey: apiKey,
+        shopOrigin: shopOrigin,
+      });
+
+      Redirect.create(app).dispatch(Redirect.Action.ADMIN_PATH, permissionUrl);
+    }
+  }
+
   render() {
     return (
       <div className="App">
@@ -17,14 +35,7 @@ class App extends Component {
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Order list
-          </a>
+            <button onClick={this.handleButtonClick}>Shopify auth</button>
         </header>
       </div>
     );
